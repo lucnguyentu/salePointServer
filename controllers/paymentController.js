27183272f01f8aaac22b47ef08d93a78a0a88a03
Huy Lương <92367180+HuyLuong2002@users.noreply.github.com>
2024-04-15 13:32:35 +0700

    done payment by card

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 dotenv.config();
 
 export const processPayment = catchAsyncErrors(async (req, res, next) => {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_LUONG);
 
     const myPayment = await stripe.paymentIntents.create({
         amount: req.body.amount,
@@ -13,11 +13,15 @@ export const processPayment = catchAsyncErrors(async (req, res, next) => {
         metadata: {
             company: '3L1K',
         },
+        payment_method: 'pm_card_visa',
+        customer: 'cus_PuDwEplYUUYjmx'
     });
+
+
 
     res.status(200).json({ success: true, client_secret: myPayment.client_secret });
 });
 
 export const sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY_LUONG });
 });
